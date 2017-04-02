e63f520b9785c58fc7a7676a1975c0e3e5da9b2d
Alex <lebedev.a@future-group.ru>
2017-04-03 02:14:51 +0300

    add trainers page

--- app/routes/api.ts
+++ app/routes/api.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 let router = express.Router();
 import controller from '../controllers';
 
-
 router.get('/kappa', controller.trainers.getTrainers);
 
 router.post('/addtrainer', controller.trainers.addTrainer);
