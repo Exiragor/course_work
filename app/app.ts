a78cc3ba8650423f42026c464020950f1f70d775
Alex <lebedev.a@future-group.ru>
2017-04-05 15:53:50 +0300

    fix routes, add Class Routes

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import * as path from 'path';
 import * as temp from 'consolidate';
-import apiRoutes from './routes/api';
+import { router } from './routes';
 import * as bodyParser from 'body-parser';
 
 let app = express();
@@ -16,7 +16,8 @@ app.set('views', path.join(__dirname +'/../views'));
 
 app.use('/public', express.static(path.join(__dirname +'/../public')));
 
-app.use('/admin', apiRoutes);
+app.use('/admin', router.getAdminRoutes());
+app.use('/dev', router.getDevRoutes());
 
 app.get('*', (req, res) => {
     res.render('index', {mess: 'Hello', text: 'Добро пожаловать!'});
---
+++ app/routes/index.ts
@@ -0,0 +1,53 @@
+import * as express from 'express';
+import controller from '../controllers';
+
+interface IRouter {
+    admin: any,
+    dev: any
+}
+
+export class Routes{
+    router: IRouter;
+
+    constructor() {
+        let admin, dev;
+        this.router = {
+            admin,
+            dev
+        };
+        for (let rout in this.router){
+            this.router[rout] = express.Router();
+        }
+    }
+
+    public getAdminRoutes() {
+        this.router.admin.get('/trainers', (req, res) => {
+            controller.trainers.getTrainers(res);
+        });
+
+        this.router.admin.post('/trainers/add', (req, res) => {
+            controller.trainers.addTrainer(req.body, res);
+        });
+
+        this.router.admin.post('/trainers/:id/edit', (req, res) => {
+            controller.trainers.editTrainer(req.params.id, req.body, res);
+        });
+
+        return this.router.admin;
+    }
+
+    public getDevRoutes() {
+        this.router.dev.get('/generate/trainers', (req, res) => {
+            controller.generator.generateTrainers();
+            res.send('Идет заполнение');
+        });
+
+        this.router.dev.get('/generate/visitors', (req, res) => {
+            controller.generator.generateVisitors();
+            res.send('Идет заполнение');
+        });
+    }
+
+}
+
+export let router = new Routes;
