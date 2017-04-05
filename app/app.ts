fad06b3fd3aacabcbc3fc98e02fe72f270dd518d
Alex <lebedev.a@future-group.ru>
2017-04-05 14:46:31 +0300

    remove hbs, add jade

--- app/app.ts
+++ app/app.ts
@@ -10,8 +10,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     
   extended: true
 })); 
-app.engine('hbs', temp.handlebars);
-app.set('view engine', 'hbs');
+app.engine('jade', temp.jade);
+app.set('view engine', 'jade');
 app.set('views', path.join(__dirname +'/../views'));
 
 app.use('/public', express.static(path.join(__dirname +'/../public')));
@@ -19,7 +19,7 @@ app.use('/public', express.static(path.join(__dirname +'/../public')));
 app.use('/admin', apiRoutes);
 
 app.get('*', (req, res) => {
-    res.render('index');
+    res.render('index', {mess: 'Hello', text: 'Добро пожаловать!'});
 });
 
 
